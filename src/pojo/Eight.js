4a767ec0537bf3b6dfbe39301a02b1dc6bdda4fa
lax <756993539@qq.com>
2020-10-26 00:37:31 +0800

    update

--- src/pojo/Eight.js
+++ src/pojo/Eight.js
@@ -1,10 +1,11 @@
 /*
  * @Description:
+ * 公式：https://baike.baidu.com/item/%E6%97%A5%E6%9F%B1%E5%85%AC%E5%BC%8F/18780172?fr=aladdin
  * @Version: 1.0.0
  * @Autor: lax
  * @Date: 2020-10-22 15:38:09
  * @LastEditors: lax
- * @LastEditTime: 2020-10-22 23:18:03
+ * @LastEditTime: 2020-10-26 00:36:02
  */
 const gz = require("./GanZhi");
 class Eight {
@@ -12,35 +13,89 @@ class Eight {
 		this.date = p.date || this.time();
 		this.year = this.getYear();
 		this.mouth = this.getMonth();
-		console.log(this.mouth);
+		this.day = this.getDay();
+		this.hour = this.getHour();
 	}
-	getYear() {
+	getYear(year) {
 		// 年份+6的个位数即对应天干
-		const year_value = this.date.getFullYear();
-		const single = year_value + 6 + "";
-		const tg_index = ~~single.slice(single.length - 1);
+		const year_value = year || this.date.getFullYear();
+		const single = year_value + 6;
+		const tg_index = this.__RightFigure(single);
 		// 年份的12余数+6对应地支
-		const remainder = (year_value % 12) + 6 + "";
-		const dz_index = ~~remainder.slice(remainder.length - 1);
+		const remainder = (year_value % 12) + 6;
+		const dz_index = this.__RightFigure(remainder);
 		return new gz(tg_index, dz_index);
 	}
 	getMonth() {
-		const mouth_index = this.date.getMonth();
+		const mouth_index = this.date.getMonth() + 1;
 		// 月份对应地支 12月对应0
-		const dz_index = mouth_index == 12 ? 0 : mouth_index + 1;
+		const dz_index = mouth_index;
 		// 年干*2+月地支的个位数为月天干
-		const year_gan = this.year.getXIndex() * 2 + dz_index + "";
-		const tg_index = ~~year_gan.slice(year_gan.length - 1);
+		const hs = this.year.x * 2 + dz_index;
+		const tg_index = this.__RightFigure(hs);
 		return new gz(tg_index, dz_index);
 	}
-	day() {
-		// const index = this.year.index + this.mouth.index + this.date.getDays();
-		// console.log(index);
+	getDay() {
+		// 实际月份
+		let mouth = this.date.getMonth() + 1;
+		// 实际年份
+		const year = this.date.getFullYear();
+		// 月份为13、14月
+		mouth = 4 > mouth > 1 ? mouth + 12 : mouth;
+		// 世纪数
+		const century = this.getCenturyCount(year);
+		// 年数 = 此纪年2月末的日干支序数
+		const x = (44 * (century - 1) + Math.floor((century - 1) / 4) + 9) % 60;
+		// 月数
+		const m =
+			(30 * (Math.pow(-1, mouth) + 1)) / 2 + Math.floor((3 * mouth - 7) / 5);
+		// figure
+		const figure = Math.floor(this.__RightFigure(year, 2) / 4);
+		//日干支序数 = 年数+月数+日期 （和大于60，则减60。1月、2月用上一年的年数）
+		let index =
+			(figure * 6 +
+				5 * (figure * 3 + (this.__RightFigure(year, 2) % 4)) +
+				x +
+				m +
+				this.date.getDate()) %
+			60;
+		index == 0 ? 60 : index;
+		return new gz(index);
 	}
-	hour() {}
+	getCenturyCount(year) {
+		return Math.floor(year / 100) + 1;
+	}
+	getHour() {
+		// 十二时辰对应十二地支
+		const _y = Math.ceil(this.date.getHours() / 2);
+		const y = _y == 12 ? 0 : _y;
+		// 根据日天干推算子时时天干
+		const single = this.day.x * 2;
+		// 再推算实际时天干
+		const _x = this.__RightFigure(single) + y;
+		const x = this.__RightFigure(_x);
+		return new gz(x, y);
+	}
+	result() {
+		return [
+			this.year.getXContent(),
+			this.year.getYContent(),
+			this.mouth.getXContent(),
+			this.mouth.getYContent(),
+			this.day.getXContent(),
+			this.day.getYContent(),
+			this.hour.getXContent(),
+			this.hour.getYContent()
+		];
+	}
 	time() {
 		return new Date();
 	}
+	__RightFigure(num, index = 1) {
+		const _num = num + "";
+		return ~~_num.slice(_num.length - index);
+	}
 }
 module.exports = Eight;
-new Eight();
+const eight = new Eight();
+console.log(eight.result());
--- src/pojo/GanZhi.js
+++ src/pojo/GanZhi.js
@@ -1,27 +1,32 @@
 /*
- * @Description:
+ * @Description: 天干地支对象
  * @Version: 1.0.0
  * @Autor: lax
  * @Date: 2020-10-22 20:15:13
  * @LastEditors: lax
- * @LastEditTime: 2020-10-22 23:18:46
+ * @LastEditTime: 2020-10-25 22:07:57
  */
 class GanZhi {
-	constructor(x = 0, y = 0) {
+	constructor(x = 0, y = 0, type = 0) {
+		// 代表传入的是干支对应数字
 		if (arguments.length == 1) {
-			this.x = this._getByIndex().x;
-			this.y = this._getByIndex().y;
+			const xy = this._getByIndex(x);
+			this.x = xy.x;
+			this.y = xy.y;
 		} else {
 			this.x = x;
 			this.y = y;
 		}
 		this.index = this._getIndex();
+		this.type = type;
 	}
-	getXIndex() {
-		return this.x;
+	// 天干 Heavenly Stems
+	hs() {
+		return this.x + 1;
 	}
-	getYIndex() {
-		return this.y;
+	// 地支 Terrestrial Branch
+	tb() {
+		return (this.x < this.y ? this.x - this.y + 12 : this.x - this.y) / 2;
 	}
 	getXContent() {
 		return this._x[this.x];
@@ -29,31 +34,23 @@ class GanZhi {
 	getYContent() {
 		return this._y[this.y];
 	}
+	// 天干地支对应的数字
 	_getIndex() {
-		const single = this.x + 1;
-		const ten = (this.x < this.y ? this.x - this.y + 12 : this.x - this.y) / 2;
-		return ten * 10 + single;
+		const hs = this.hs();
+		const tb = this.tb();
+		return tb * 10 + hs;
 	}
-	_getByIndex() {
-		const str = this.x + "";
-		const x = ~~str.slice(str.length - 1) - 1;
-		const y = ~~str.slice(0, str.length - 1);
+	_getByIndex(xy) {
+		const str = xy + "";
+		const _x = ~~str.slice(str.length - 1);
+		const _y = ~~str.slice(0, str.length - 1);
+		const x = _x - 1;
+		const y = x - _y * 2 < 0 ? x - _y * 2 + 12 : x - _y * 2;
+
 		return { x, y };
 	}
+	_x = ["甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"];
+	_y = ["子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"];
 }
-GanZhi._x = ["甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"];
-GanZhi._y = [
-	"子",
-	"丑",
-	"寅",
-	"卯",
-	"辰",
-	"巳",
-	"午",
-	"未",
-	"申",
-	"酉",
-	"戌",
-	"亥"
-];
+
 module.exports = GanZhi;
