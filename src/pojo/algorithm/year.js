f332f239506bf5f314eb91ab5ffca4a28a854a4e
lax <756993539@qq.com>
2022-05-29 11:51:24 +0800

    update

--- src/pojo/algorithm/year.js
+++ src/pojo/algorithm/year.js
@@ -4,17 +4,14 @@
  * @Author: lax
  * @Date: 2021-10-15 23:24:59
  * @LastEditors: lax
- * @LastEditTime: 2021-10-15 23:42:40
+ * @LastEditTime: 2022-05-29 11:50:08
  * @FilePath: \taobi\src\pojo\algorithm\year.js
  */
-const _ = require("@/tools/index");
 
 module.exports = (year) => {
 	// 年份个位数=>天干 补6顺位
-	const single = year + 6;
-	const x = _.rightFigure(single);
+	const x = (year + 6) % 10;
 	// 年份的12余数=>地支 补8顺位
-	const remainder = (year % 12) + 8;
-	const y = remainder % 12;
+	const y = (year + 8) % 12;
 	return { x, y };
 };
