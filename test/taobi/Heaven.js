9fa2162e138c9b2b7b36a4840cf7511e4f5a83f8
lax <756993539@qq.com>
2022-09-07 07:32:49 +0800

    remove

--- test/taobi/Heaven.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Description:
- * @Version: 1.0.0
- * @Author: lax
- * @Date: 2020-12-15 22:26:04
- * @LastEditors: lax
- * @LastEditTime: 2021-02-10 20:38:16
- * @FilePath: \taobi\test\taobi\Heaven.js
- */
