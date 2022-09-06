d30bddc0b4879595617459496731bae039c9b66d
lax <756993539@qq.com>
2022-09-07 07:33:16 +0800

    move

---
+++ test/earth/earth.spec.js
@@ -0,0 +1,26 @@
+/*
+ * @Description:
+ * @Version: 1.0.0
+ * @Author: lax
+ * @Date: 2020-12-11 00:57:08
+ * @LastEditors: lax
+ * @LastEditTime: 2022-09-07 07:29:01
+ * @FilePath: \taobi\test\tao.spec.js
+ */
+const surpriseCeremony = require("./surpriseCeremony");
+const Tao = require("@/pojo/taobi/TheArtOfBecomingInvisible.js");
+const now = "壬寅壬寅辛亥丙申";
+describe("奇门摆盘-第三步:地盘布三奇六仪", () => {
+	for (let i = -9; i <= 9; i++) {
+		if (i) {
+			const tao = new Tao(now, i);
+			const name = `${i < 0 ? "_" : ""}list${Math.abs(i)}`;
+			const round = surpriseCeremony[name];
+			it(`用局:${i > 0 ? "阳" : "阴"}${Math.abs(i)},结果:${round}`, () => {
+				tao.acquired.map((palace, index) => {
+					expect(palace.getECS()[0]).toBe(round[index]);
+				});
+			});
+		}
+	}
+});
