dd0befacef102e4c906652141e7824c9d2f217e3
lax <laxlax@tutanota.com>
2023-04-20 22:17:35 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const Palace = require("@/pojo/taobi/Palace");
+const TheArtOfBecomingInvisible = require("@/pojo/taobi/TheArtOfBecomingInvisible");
+
+module.exports = {
+	Palace,
+	TheArtOfBecomingInvisible,
+};
