08c5cbdc4f3f1b85a54dafea5c56a3a97cab5246
lax <laxlax@tutanota.com>
2023-04-29 08:46:25 +0800

    update

--- src/pojo/taobi/Palace.js
+++ src/pojo/taobi/Palace.js
@@ -4,11 +4,21 @@
  * @Author: lax
  * @Date: 2020-10-27 16:35:18
  * @LastEditors: lax
- * @LastEditTime: 2022-09-10 11:55:30
+ * @LastEditTime: 2023-04-29 08:31:30
  */
-const { acquired, num } = require("@/pojo/Tao.js");
+const {
+	acquired,
+	num,
+	door,
+	star,
+	divinity,
+	ceremony,
+	surprise,
+	celestialStems,
+	terrestrialBranches,
+} = require("@/pojo/Tao.js");
 const inspect = Symbol.for("nodejs.util.inspect.custom");
-
+const surpriseCeremony = ceremony.concat(surprise);
 class Palace {
 	constructor(index) {
 		/**
@@ -22,95 +32,149 @@ class Palace {
 		/**
 		 * 天盘天干
 		 */
-		this.hs = [];
+		this.hcs = [];
 		/**
 		 * 地盘天干
 		 */
-		this._hs = "";
+		this.ecs = [];
 		/**
 		 * 星
 		 */
-		this.star = "";
+		this.star = null;
 		/**
 		 * 门
 		 */
-		this.door = "";
+		this.door = null;
 		/**
 		 * 神
 		 */
-		this.divinity = "";
+		this.divinity = null;
+		/**
+		 * 原始天干
+		 */
+		this.cs = [];
+		/**
+		 * 原始地支
+		 */
+		this.tb = [];
 	}
 
 	// ######### earths celestial stems #########
-	setEarthsCelestialStems(hs) {
-		this._hs = hs;
+	setEarthsCelestialStems(index) {
+		this.ecs = index;
 	}
 
-	setECS(hs) {
-		this.setEarthsCelestialStems(hs);
+	setECS(index) {
+		this.setEarthsCelestialStems(index);
 	}
 
-	getEarthsCelestialStems() {
-		return this._hs;
+	getEarthsCelestialStems(is = false) {
+		return is ? this.ecs.map((o) => surpriseCeremony[o]) : this.ecs;
 	}
 
-	getECS() {
-		return this.getEarthsCelestialStems();
+	getECS(is) {
+		return this.getEarthsCelestialStems(is);
 	}
 
 	// ######### heavens celestial stems #########
-	setHeavensCelestialStems(hs) {
-		this.hs = hs;
+	setHeavensCelestialStems(index) {
+		this.hcs = index;
 	}
 
-	setHCS(hs) {
-		this.setHeavensCelestialStems(hs);
+	setHCS(index) {
+		this.setHeavensCelestialStems(index);
 	}
 
-	getHeavensCelestialStems() {
-		return this.hs;
+	getHeavensCelestialStems(is = false) {
+		return is ? this.hcs.map((o) => surpriseCeremony[o]) : this.hcs;
 	}
 
-	getHCS() {
-		return this.getHeavensCelestialStems();
+	getHCS(is) {
+		return this.getHeavensCelestialStems(is);
 	}
 
 	// ######### star #########
-	setStar(star) {
-		this.star = star;
+	setStar(index) {
+		this.star = index;
 	}
 
-	getStar() {
-		return this.star;
+	getStar(is = false) {
+		if (this.star === null) return "";
+		return is ? star[this.star] : this.star;
 	}
 
 	// ######### door #########
-	setDoor(door) {
-		this.door = door;
+	setDoor(index) {
+		this.door = index;
 	}
 
-	getDoor() {
-		return this.door;
+	getDoor(is = false) {
+		if (this.door === null) return "";
+		return is ? door[this.door] : this.door;
 	}
 
 	// ######### divinity #########
-	setDivinity(divinity) {
-		this.divinity = divinity;
+	setDivinity(index) {
+		this.divinity = index;
 	}
 
-	getDivinity() {
-		return this.divinity;
+	getDivinity(is = false) {
+		if (this.divinity === null) return "";
+		return is ? divinity[this.divinity] : this.divinity;
+	}
+
+	// ######### palace #########
+	getPalace(is = false) {
+		return is ? acquired[this.index] : this.index;
+	}
+
+	setPalace(index) {
+		this.index = index;
+	}
+
+	// ######### celestial stems #########
+	setCelestialStems(index) {
+		this.cs = index;
+	}
+
+	setCS(index) {
+		this.setCelestialStems(index);
+	}
+
+	getCelestialStems(is = false) {
+		return is ? this.cs.map((o) => celestialStems[o]) : this.cs;
+	}
+
+	getCS(is) {
+		return this.getCelestialStems(is);
+	}
+
+	// ######### terrestrial branches #########
+	setTerrestrialBranches(index) {
+		this.tb = index;
+	}
+
+	setTB(index) {
+		this.setTerrestrialBranches(index);
+	}
+
+	getTerrestrialBranches(is = false) {
+		return is ? this.tb.map((o) => terrestrialBranches[o]) : this.tb;
+	}
+
+	getTB(is) {
+		return this.getTerrestrialBranches(is);
 	}
 
 	toCanvas() {
 		return [
-			[this.divinity, "", ""],
+			[this.getDivinity(true), "", ""],
+			[this.getDoor(true), "", `${this.getHCS(true)}`],
 			[
-				this.door,
-				"",
-				`${this.hs[0] ? this.hs[0] : ""}${this.hs[1] ? this.hs[1] : ""}`,
+				this.getStar(true),
+				`${this.getPalace(true)}${num[this.index]}`,
+				`${this.getECS(true)}`,
 			],
-			[this.star, `${acquired[this.index]}${num[this.index]}`, this._hs],
 		];
 	}
 
